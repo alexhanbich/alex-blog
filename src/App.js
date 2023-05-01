5bcbf2e04ba4bd216158e9ae02523bb081ed47fe
Alex Kang <alexhanbich@gmail.com>
2023-05-01 16:51:56 -0600

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import PostItem from "./components/PostItem";
+import postData from "./data/postData";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="block mx-auto w-full sm:w-[640px] mx-6 font-montserrat">
       <Navbar />
+      <div className="w-full mt-8 px-4">
+        {postData.map((item) => (
+          <PostItem title={item.title} details={item.details} />
+        ))}
+      </div>
+      <Footer />
     </div>
+
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,10 @@
 import React from "react";
-import { BiSearch } from "react-icons/bi";
 
 function Navbar() {
   return (
     <div>
       <nav className="w-full flex flex-col bg-primary bg-opacity-40">
-        <h1 className="self-center text-4xl m-4">Alex's Blog</h1>
-        <ul className="flex relative justify-between list-none">
-          <div class="search-box">
-            <input
-              class="search-text"
-              type="text"
-              placeholder="Search Anything"
-            />
-            <a href="#" class="search-btn">
-              <BiSearch />
-            </a>
-          </div>
-        </ul>
+        <h1 className="self-center text-4xl my-12">Alex's Blog</h1>
       </nav>
     </div>
   );
